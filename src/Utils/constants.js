0669a0e2d1c7e188961e85b24c68d13254af9ac7
anuragdubey2004 <anuragdubey635@.in>
2024-06-20 10:32:22 +0530

    UPDATE

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,3 +1,5 @@
+const TMDB_Authorization =  process.env.REACT_APP_TMDB_TOKEN;
+
 export const LOGO_URL =
    "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
